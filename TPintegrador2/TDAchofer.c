d34946f1dfa9bbe8bc7ef7afa4ecb2b7a115bbf7
xnanomarx <marianoramirez0714@gmail.com>
2022-09-26 16:52:55 -0300

    prueba

--- TPintegrador2/TDAchofer.c
+++ TPintegrador2/TDAchofer.c
@@ -3,6 +3,9 @@
 #include "TDAchofer.h"
 
 choferPtr crearChofer(char* nombre, char* apellido, domicilioPtr, int cuil){
+    choferPtr chof=(choferPtr)obtenerMemoria(sizeof(chofer));
+    chof->nombre,apellido,domicilioPtr,cuil;
+
 
 
 
